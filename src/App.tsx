5b985e5a25c009164a544d507d0d8ffa886917c2
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-02-24 15:41:34 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from './components/Navbar';
+import Hero from './components/Hero';
+
+function App() {
+  return (
+    <div className=" bg-bgClr dark:bg-bgClrDark">
+      { /* Add your code here */}
+      <NavBar />
+      <Hero />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className='h-[90.6vh]'>
+            <h1>Hero</h1>
+        </div>
+    )
+}
+
+export default Hero
