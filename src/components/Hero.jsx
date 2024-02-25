826e0a402ccddad973abdcc28282ed70df6a7980
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-02-26 00:44:05 +0100

    doing hard things

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,18 @@ import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <div className="h-[90.6vh] items-center justify-center flex w-[80%] text-textClr mx-auto gap-16 dark:text-textClrDark flex-col lg:flex-row">
-      <div className="lg:w-[50%] w-[90%] justify-center items-start flex flex-col">
+      <motion.div
+        initial={{ x: -100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{
+          delay: 0.1,
+          x: { type: "spring", stiffness: 50 },
+          opacity: { duration: 0.8 },
+          ease: "easeIn",
+          duration: 1,
+        }}
+        className="lg:w-[50%] w-[90%] justify-center items-start flex flex-col"
+      >
         <h1 className=" font-light text-sm lg:text-3xl">
           Hi{" "}
           <span className=" text-accentClr dark:text-accentClrDark">I'm</span>
@@ -35,13 +46,24 @@ const Hero = () => {
             ]}
           />
         </h1>
-      </div>
-      <div className="lg:w-[50%] items-center flex justify-center ">
+      </motion.div>
+      <motion.div
+        initial={{ x: 100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{
+          delay: 0.1,
+          x: { type: "spring", stiffness: 200 },
+          opacity: { duration: 0.8 },
+          ease: "easeIn",
+          duration: 1,
+        }}
+        className="lg:w-[50%] items-center flex justify-center "
+      >
         <div
           className=" lg:w-[440px] lg:h-[440px] w-[280px] h-[280px] border border-accentClr dark:border-accentClrDark bg-center bg-cover rounded-full"
           style={{ backgroundImage: `url("/Sanya Ololade.jpg")` }}
         ></div>
-      </div>
+      </motion.div>
       {/**/}
     </div>
   );
