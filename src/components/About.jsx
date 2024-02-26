c45e3d93e7808976706b02d041d13e1e832ee130
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-02-26 22:32:19 +0100

    doing hard things

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -84,7 +84,19 @@ const About = () => {
     <div className=" w-[90%] lg:w-[90%] text-textClr flex mt-12 pb-12 dark:text-textClrDark justify-center items-center mx-auto">
       <div className="  flex flex-col gap-6 lg:w-[80%] w-[90%] mx-auto">
         <div className=" w-full lg:grid lg:grid-cols-5 lg:auto-cols-[300px] gap-6">
-          <div className=" dark:bg-darkgbg bg-lightgbg py-5 px-8 lg:col-span-3 w-full  rounded-xl flex flex-col lg:gap-2 gap-4  ">
+          <motion.div
+            initial={{ x: -100, opacity: 0 }}
+            viewport={{ once: true }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{
+              delay: 0.8,
+              x: { type: "spring", stiffness: 200 },
+              opacity: { duration: 2 },
+              ease: "easeIn",
+              duration: 2,
+            }}
+            className=" dark:bg-darkgbg bg-lightgbg py-5 px-8 lg:col-span-3 w-full  rounded-xl flex flex-col lg:gap-2 gap-4  "
+          >
             <h1 className="text-textClr  dark:text-textClrDark text-xl font-medium">
               About{" "}
               <span className=" text-accentClr dark:text-accentClrDark font-medium">
@@ -99,30 +111,43 @@ const About = () => {
               currently seeking a challenging position where I can contribute to
               exciting projects.
             </p>
-          </div>
-          <div className="dark:bg-darkgbg bg-lightgbg justify-center lg:col-span-2 items-center flex rounded-xl">
-            <motion.div
-              initial={{ x: 100, opacity: 0 }}
-              whileInView={{ x: 0, opacity: 1 }}
-              transition={{
-                delay: 0.1,
-                x: { type: "spring", stiffness: 200 },
-                opacity: { duration: 0.8 },
-                ease: "easeIn",
-                duration: 1,
-              }}
-              className="lg:w-[50%] items-center flex justify-center "
-            >
+          </motion.div>
+          <motion.div
+            initial={{ x: 100, opacity: 0 }}
+            viewport={{ once: true }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{
+              delay: 0.1,
+              x: { type: "spring", stiffness: 200 },
+              opacity: { duration: 0.8 },
+              ease: "easeIn",
+              duration: 1,
+            }}
+            className="dark:bg-darkgbg bg-lightgbg justify-center lg:col-span-2 items-center flex rounded-xl"
+          >
+            <div className="lg:w-[50%] items-center flex justify-center ">
               <div
                 className=" lg:w-[160px] lg:h-[160px] w-[200px] h-[200px] border border-accentClr dark:border-accentClrDark bg-center bg-cover rounded-full"
                 style={{ backgroundImage: `url("/Sanya Ololade.jpg")` }}
               ></div>
-            </motion.div>
-          </div>
+            </div>
+          </motion.div>
         </div>
         <div className=" w-full lg:grid lg:grid-cols-5 lg:auto-cols-[300px] gap-6 ">
           <div className=" w-full lg:grid lg:grid-cols-2 flex lg:auto-cols-[300px] gap-6 flex-row mb-6 lg:mb-0  lg:col-span-2 lg:gap-6 ">
-            <div className=" rounded-2xl dark:bg-darkgbg bg-lightgbg flex flex-col justify-center w-full items-center">
+            <motion.div
+              initial={{ scale: 0, opacity: 0 }}
+              viewport={{ once: true }}
+              whileInView={{ scale: 1, opacity: 1 }}
+              transition={{
+                delay: 0.1,
+                scale: { type: "spring", stiffness: 200 },
+                opacity: { duration: 0.8 },
+                ease: "easeIn",
+                duration: 1,
+              }}
+              className=" rounded-2xl dark:bg-darkgbg bg-lightgbg flex flex-col justify-center w-full items-center"
+            >
               <h1 className=" text-6xl">
                 2
                 <span className=" text-accentClr dark:text-accentClrDark">
@@ -132,8 +157,20 @@ const About = () => {
               <p className=" text-sm font-thin text-center">
                 Years of Experience
               </p>
-            </div>
-            <div className=" rounded-2xl gap-2 dark:bg-darkgbg bg-lightgbg w-full flex flex-col justify-center items-center">
+            </motion.div>
+            <motion.div
+              initial={{ scale: 0, opacity: 0 }}
+              viewport={{ once: true }}
+              whileInView={{ scale: 1, opacity: 1 }}
+              transition={{
+                delay: 0.1,
+                scale: { type: "spring", stiffness: 200 },
+                opacity: { duration: 0.8 },
+                ease: "easeIn",
+                duration: 1,
+              }}
+              className=" rounded-2xl gap-2 dark:bg-darkgbg bg-lightgbg w-full flex flex-col justify-center items-center"
+            >
               <h1 className=" text-6xl">
                 8
                 <span className=" text-accentClr dark:text-accentClrDark">
@@ -143,24 +180,60 @@ const About = () => {
               <p className=" text-textClr dark:text-textClrDark text-sm font-thin">
                 Projects
               </p>
-            </div>
+            </motion.div>
           </div>
 
-          <div className=" w-full rounded-2xl dark:bg-darkgbg bg-lightgbg lg:col-span-3 justify-center items-center flex px-6 py-3">
+          <motion.div
+            initial={{ x: 100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{
+              delay: 20,
+              x: { type: "spring", stiffness: 200 },
+              opacity: { duration: 0.8 },
+              ease: "easeIn",
+              duration: 1,
+            }}
+            className=" w-full rounded-2xl dark:bg-darkgbg bg-lightgbg lg:col-span-3 justify-center items-center flex px-6 py-3"
+          >
             Full-stack dev bringing magic to the front-end with React, Next.js,
             Tailwind CSS, Framer Motion, and DaisyUI. Powerful back-end with
             Node.js and Express.js. Always up for a learning adventure!
-          </div>
+          </motion.div>
         </div>
         <div className=" md:w-full flex flex-col md:grid md:grid-cols-5 md:auto-cols-[200px] gap-6 ">
-          <div className=" gap-2 rounded-2xl dark:bg-darkgbg bg-lightgbg md:col-span-2 flex py-2 px-4 justify-center items-center">
+          <motion.div
+            initial={{ scale: 0, opacity: 0 }}
+            viewport={{ once: true }}
+            whileInView={{ scale: 1, opacity: 1 }}
+            transition={{
+              delay: 0.1,
+              scale: { type: "spring", stiffness: 200 },
+              opacity: { duration: 0.8 },
+              ease: "easeIn",
+              duration: 1,
+            }}
+            className=" gap-2 rounded-2xl dark:bg-darkgbg bg-lightgbg md:col-span-2 flex py-2 px-4 justify-center items-center"
+          >
             {socialIcons.map((icon, i) => (
               <a href={icon.link} target="_blank" key={i} className=" ">
                 <img src={icon.url} alt="" className=" size-12" />
               </a>
             ))}
-          </div>
-          <div className=" rounded-2xl dark:bg-darkgbg bg-lightgbg md:col-span-2 flex justify-center items-center gap-4 py-2 px-5">
+          </motion.div>
+          <motion.div
+            initial={{ scale: 0, opacity: 0 }}
+            viewport={{ once: true }}
+            whileInView={{ scale: 1, opacity: 1 }}
+            transition={{
+              delay: 0.1,
+              scale: { type: "spring", stiffness: 200 },
+              opacity: { duration: 0.8 },
+              ease: "easeIn",
+              duration: 1,
+            }}
+            className=" rounded-2xl dark:bg-darkgbg bg-lightgbg md:col-span-2 flex justify-center items-center gap-4 py-2 px-5"
+          >
             {Stack.map((each, id) => (
               <div key={id}>
                 <img
@@ -170,25 +243,39 @@ const About = () => {
                 />
               </div>
             ))}
-          </div>
-          <div className=" rounded-2xl dark:bg-darkgbg bg-lightgbg h-[160px] gap-3 justify-center items-center flex px-2 pt-3">
+          </motion.div>
+          <motion.div
+            initial={{ y: 100, opacity: 0 }}
+            viewport={{ once: true }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{
+              delay: 0.1,
+              y: { type: "spring", stiffness: 200 },
+              opacity: { duration: 0.8 },
+              ease: "easeIn",
+              duration: 1,
+            }}
+            className=" rounded-2xl dark:bg-darkgbg bg-lightgbg h-[160px] gap-3 justify-center items-center flex px-2 pt-3"
+          >
             <div className=" flex flex-col justify-center items-center">
               <img src={Mongodb} alt="" className="flex size-8" />
-              <p>M</p>
+              <p className=" text-textClr dark:text-textClrDark">M</p>
             </div>
-            <div className=" flex flex-col justify-center items-center">
-              <img src={expressjs} alt="" className="flex size-8" />
-              <p>E</p>
+            <div className=" flex gap-1 flex-col justify-center items-center">
+              <h1 className=" text-textClr dark:text-textClrDark text-center flex text-xl ">
+                ex
+              </h1>
+              <p className=" text-textClr dark:text-textClrDark">E</p>
             </div>
             <div className=" flex flex-col justify-center items-center">
               <img src={ReactJs} alt="" className="flex size-8" />
-              <p>R</p>
+              <p className=" text-textClr dark:text-textClrDark">R</p>
             </div>
             <div className=" flex flex-col justify-center items-center">
               <img src={NodeJs} alt="" className="flex size-8" />
-              <p>N</p>
+              <p className=" text-textClr dark:text-textClrDark">N</p>
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
