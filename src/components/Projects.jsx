75f13e7e49371946aa72cc2281be6331fc080c01
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-03-21 17:12:02 +0100

    doing hard thjings

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,9 @@ const Projects = () => {
   return (
     <div className=" w-full">
       <div className=" w-[80%] flex lg:flex-row gap-8 ">
-        <div className="">{/* <img src="" alt="" /> */}</div>
+        <div className="">
+          <img src="" alt="" />
+        </div>
       </div>
     </div>
   );
