68b8defaab518750feba12e198cafcc8a6eaf3e2
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-06-27 14:39:42 +0100

    updated the project section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -177,7 +177,7 @@ const About = () => {
               className=" rounded-2xl gap-2 dark:bg-darkgbg bg-lightgbg w-full flex flex-col justify-center items-center"
             >
               <h1 className=" text-6xl">
-                8
+                5
                 <span className=" text-accentClr dark:text-accentClrDark">
                   +
                 </span>
